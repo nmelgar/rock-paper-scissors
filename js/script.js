2a5a654749d8ce91e9232b89c8ef05b9ee7270ab
nmelgar <nmelgar16013@gmail.com>
2021-10-19 10:26:01 -0500

    19-10-10:25--changes in JS, showing what the computer chose

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 //User
-var userInput = prompt("What you choose, Rock, Paper or Scissors?");
+// var userInput = prompt("What you choose, Rock, Paper or Scissors?");
 //console.log(userInput.toUpperCase());
 
 //Computer
 const computerOptions = ["ROCK", "PAPER", "SCISSORS"];
 computerInput = computerOptions[Math.floor(Math.random() * computerOptions.length)];
-//console.log("Computer chose: " + computerInput);
+// console.log("Computer chose: " + computerInput);
 
-var userChose = document.getElementById('user-chose');
-userChose.textContent = "User chose: " + userInput.toUpperCase();
+var userChose = document.getElementById('rock-button', 'paper-button', 'scissors-button');
+
 
 var computerChose = document.getElementById('computer-chose');
 computerChose.textContent = "Computer chose: " + computerInput;
@@ -16,15 +16,15 @@ computerChose.textContent = "Computer chose: " + computerInput;
 var gameResult = document.getElementById('result');
 // gameResult.textContent = gameRound;
 
-function playRound() {
-  if (userChose == "ROCK" && computerInput == "PAPER") {
-    console.log("Computer Wins");
-  } else if (userInput == "ROCK" && computerInput == "SCISSORS") {
-    document.write("User Wins!");
-  } else if (userInput == "ROCK" && computerInput == "ROCK") {
-    document.write("No Winner!");
-  }
+// function playRound() {
+//   if (userChose == "ROCK" && computerInput == "PAPER") {
+//     console.log("Computer Wins");
+//   } else if (userInput == "ROCK" && computerInput == "SCISSORS") {
+//     document.write("User Wins!");
+//   } else if (userInput == "ROCK" && computerInput == "ROCK") {
+//     document.write("No Winner!");
+//   }
 
-}
+// }
 
 // var gameRound = playRound();
